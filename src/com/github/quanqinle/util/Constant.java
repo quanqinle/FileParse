de9acce59f1d62a76e26896e8ddf77747618b2a9
quanqinle <quanqinle@gmail.com>
2017-08-09 22:02:42 +0800

    add json/csv parsing demo

--- src/com/github/quanqinle/util/Constant.java
+++ src/com/github/quanqinle/util/Constant.java
@@ -1,4 +1,14 @@
 package com.github.quanqinle.util;
 
-public class Constant {
+/**
+ * global constant variables
+ *
+ * @author 权芹乐
+ *
+ */
+public final class Constant {
+
+    public static final String ROOT = System.getProperty("user.dir");
+
+    public static final String RAW_FILE_DIR = "rawlog";
 }
