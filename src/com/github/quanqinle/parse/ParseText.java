f905ce26e15de3757678c7f0cf7cfd4ac514494f
权芹乐 <qlquan@zhcpa.cn>
2017-08-11 17:40:01 +0800

    add text parse to handle novel

--- src/com/github/quanqinle/parse/ParseText.java
+++ src/com/github/quanqinle/parse/ParseText.java
@@ -3,8 +3,10 @@ package com.github.quanqinle.parse;
 import com.github.quanqinle.util.Constant;
 import com.github.quanqinle.util.FileIOUtils;
 import com.google.common.base.Strings;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,7 +17,8 @@ import java.util.List;
 public class ParseText {
 
     public static void main(String[] args) {
-        trimTrailings("animalfarm.txt");
+    	String newFilePath = String.join(File.separator, Constant.RAW_FILE_DIR, "AnimalFarm-4.txt");
+        FileIOUtils.writeFileFromString(newFilePath, trimTrailings("AnimalFarm.txt"));
     }
 
     /**
@@ -27,12 +30,41 @@ public class ParseText {
         String strResult = "";
         for (String line : strs) {
             if (!Strings.isNullOrEmpty(line)) {
-                //TODO
-
-            } else if (line.isEmpty()) {
+                if (NumberUtils.isDigits(line)) {
+					continue;
+				}
+                
+                strResult += handleEndChar(line);
+            } /*else if (line.isEmpty()) {
                 strResult += line;
-            }
+            }*/
         }
         return strResult;
     }
+    
+    /**
+     * 处理改行是否需要换行<br>
+     * 仅适用于英文
+     * 
+     * @param line
+     * @return
+     */
+    public static String handleEndChar(String line) {
+    	
+    	String[] romanNums = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVII","XLIX","L"};
+    	String[] punctuations = {".", "'", ":", "’"};
+    	
+    	if (Arrays.asList(romanNums).contains(line)) {
+			return '\n' + line + '\n';
+		}
+    	
+    	String endChar = line.substring(line.length()-1);
+    	if (line.endsWith("-")) {
+			return line.substring(0, line.length() - 1);
+		} else if (Arrays.asList(punctuations).contains(endChar)) {
+			return line + '\n';
+		}
+    	
+		return line + " ";
+	}
 }
